bfb91570164f8fd308c4d76ea95eec217e694967
xiaolin <462441355@qq.com>
2021-04-17 21:04:13 +0800

    * 引入ui

--- src/main.js
+++ src/main.js
@@ -4,6 +4,17 @@ import router from './router'
 import store from './store'
 import './plugins/element.js'
 
+import 'ant-design-vue/dist/antd.css';
+import Antd from 'ant-design-vue';
+
+// 剪切板
+import VueClipboard from 'vue-clipboard2'
+
+// vue 粘贴板复制插件
+VueClipboard.config.autoSetContainer = true
+Vue.use(VueClipboard)
+Vue.use(Antd)
+
 Vue.config.productionTip = false
 
 new Vue({
