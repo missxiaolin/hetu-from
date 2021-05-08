06511d5da1ad11337d52f5bcefe8cf6009975778
xiaolin <462441355@qq.com>
2021-05-08 09:55:50 +0800

    * changelog 组件编写

--- src/components/changelog/changelog.vue
+++ src/components/changelog/changelog.vue
@@ -1,16 +1,23 @@
 <template>
-    <div>changelog</div>
+  <div class="page-changelog">
+    <change-log></change-log>
+  </div>
 </template>
 
 <script>
+import ChangeLog from "../../../CHANGELOG.zh-CN.md";
+
 export default {
-    name: 'changelog',
-    mounted() {
-        
-    }
-}
+  name: "changelog",
+  components: {
+    ChangeLog,
+  },
+  mounted() {
+    // const changeLog = this.$refs.changeLog;
+    // const changeLogNodes = changeLog.$el.children;
+  },
+};
 </script>
 
 <style scoped lang="scss">
-
 </style>
