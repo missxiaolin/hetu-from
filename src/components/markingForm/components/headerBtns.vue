ad401ee9689aa19dda12351271093aed9fec8151
xiaolin <462441355@qq.com>
2021-05-13 21:05:29 +0800

    * 增加

---
+++ src/components/markingForm/components/headerBtns.vue
@@ -0,0 +1,38 @@
+<template>
+  <div></div>
+</template>
+
+<script>
+export default {
+  props: {},
+  data() {
+    return {
+      tempKey: "eboss",
+      tempList: [
+        {
+          label: "eboss",
+          value: "eboss",
+        },
+        {
+          label: "hydra",
+          value: "hydra",
+        },
+        {
+          label: "普通Element-UI",
+          value: "element",
+        },
+        {
+          label: "Test React",
+          value: "ant",
+        },
+      ],
+    };
+  },
+  created() {},
+  mounted() {},
+  methods: {},
+};
+</script>
+
+<style scoped lang="scss">
+</style>
