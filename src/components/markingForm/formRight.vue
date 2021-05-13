44c4ef30b5a6c536b7877db089e52a25d5d96705
xiaolin <462441355@qq.com>
2021-05-13 13:44:10 +0800

    * add

--- src/components/markingForm/formRight.vue
+++ src/components/markingForm/formRight.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="right-box">
-    <el-tabs v-model="activeName" :stretch="true" style="padding: 20px;">
-      <el-tab-pane label="字段属性" name="widget" style="height: 60px;">
+    <el-tabs v-model="activeName" :stretch="true" style="padding: 20px">
+      <el-tab-pane label="字段属性" name="widget" style="height: 60px">
         <span>当前选中元素已添加的属性列表props：</span>
         <ul v-if="data.highType === '4'">
           <li
@@ -50,66 +50,148 @@
           </div>
           <div v-for="(item, index) in selectedWidgetFn" :key="index">
             <!-- 点击出现弹窗，选中方法 template -->
-            {{item.body}}
-            <el-button @click="seletFnTemplate(item)">{{item.name}} - {{item.fn}}</el-button>
+            {{ item.body }}
+            <el-button @click="seletFnTemplate(item)"
+              >{{ item.name }} - {{ item.fn }}</el-button
+            >
           </div>
         </template>
       </el-tab-pane>
     </el-tabs>
-    <fnListDialog ref="fnListDialog" :data="data" :fnData="showFnListSelectItem"></fnListDialog>
+    <fnListDialog
+      ref="fnListDialog"
+      :data="data"
+      :fnData="showFnListSelectItem"
+    ></fnListDialog>
     <el-dialog
       title="设置组件pros属性中的props"
       :visible.sync="showWidgetProps"
-      :close-on-click-modal="false">
-      <eb-table
-        v-bind="tableProps"
-        selectType="checkbox"
-      ></eb-table>
+      :close-on-click-modal="false"
+    >
+      <eb-table v-bind="tableProps" selectType="checkbox"></eb-table>
     </el-dialog>
     <el-dialog
       title="设置组件table属性中的props"
       :visible.sync="showTableProps"
       :close-on-click-modal="false"
-      width="70%">
+      width="70%"
+    >
       <table-props-list :data="data"></table-props-list>
     </el-dialog>
   </div>
 </template>
 
 <script>
-import { mapGetters } from 'vuex'
-import { addDraggerWidget } from '../tool/tool.js'
-import fnListDialog from './components/fnListDialog'
-import TablePropsList from './components/tablePropsList'
+import { mapGetters } from "vuex";
+import { addDraggerWidget } from "../tool/tool.js";
+import fnListDialog from "./components/fnListDialog";
+import TablePropsList from "./components/tablePropsList";
 
 export default {
   name: "formRight",
   components: {
     fnListDialog,
-    TablePropsList
+    TablePropsList,
   },
   mixins: [],
   props: {
     data: {
       type: Object,
       default: () => {
-        return {}
-      }
-    }
+        return {};
+      },
+    },
   },
-  data () {
+  data() {
     return {
       // 属性控制tab切换：first和second
-      activeName: 'widget',
+      activeName: "widget",
       rowCount: 0, // row 行数
       // 中间选中组件，右边展示他们的字段属性
       widgetFormSelect: {}, // 默认是对象
       showFnListSelectItem: {}, // 选中的方法对象
       widgetProps: [], // props列表
-      widgetKey: '', // 记录当前选择的
+      widgetKey: "", // 记录当前选择的
       showWidgetProps: false,
-      showTableProps: false // 显示table列表
-    }
+      showTableProps: false, // 显示table列表
+    };
+  },
+  watch: {
+    data: {
+      handler(val) {
+        this.rowCount = val.children.length;
+      },
+      deep: true,
+    },
+    // 中间选中的组件数据，传递给下一个组件
+    selectWidget: {
+      handler(val) {
+        this.widgetFormSelect = val;
+        // 每次元素变化，就获取一次列表
+        if (this.widgetKey !== val.item.key) {
+          this.widgetKey = val.item.key;
+          this.handlerWidgetSetProps();
+        }
+      },
+      deep: true,
+    },
+  },
+  computed: {
+    ...mapGetters(['selectWidget']),
+    ...mapGetters(['selectedWidgetFn']),
+    ...mapGetters(['selectTemplate']),
+    tableProps() {
+      // :columns="data.children[0].props.columns" :data="data.children[0].props.dataList"
+      let columns = [
+        {
+          label: "参数",
+          prop: "key",
+        },
+        {
+          label: "说明",
+          prop: "tetx",
+        },
+        {
+          label: "类型",
+          prop: "type",
+        },
+        {
+          label: "可选值",
+          prop: "values",
+        },
+        {
+          label: "默认值",
+          prop: "default",
+          scope: "default",
+        },
+      ];
+      let data = [];
+      if (this.data.props) {
+        let props = this.data.props || [];
+        props.forEach((item) => {
+          if (item.key === "props") {
+            data = item.children || [];
+          }
+        });
+      }
+      if (this.data.children && this.data.children.length > 0) {
+        let tabAST = this.data.children[0];
+        if (tabAST.props && tabAST.props.length > 0) {
+          tabAST.props.forEach((item) => {
+            if (item.key === "columns") {
+              columns = item.default || [];
+            }
+            if (item.key === "data") {
+              data = item.default || [];
+            }
+          });
+        }
+      }
+      return {
+        columns: columns,
+        data: data,
+      };
+    },
   },
 };
 </script>
