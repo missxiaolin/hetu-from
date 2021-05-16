e1c52fa9832199e3d0be6fb847e1a50ed65f9ed2
xiaolin <462441355@qq.com>
2021-05-16 16:14:38 +0800

    * add

--- src/components/markingForm/index.vue
+++ src/components/markingForm/index.vue
@@ -22,6 +22,9 @@
         <el-button type="primary" size="medium" @click="clearJson"
           >清空数据</el-button
         >
+        <el-button type="primary" size="medium" @click="getJson"
+          >复制JSON</el-button
+        >
       </el-header>
       <el-main>
         <!-- 这里是主程序 -->
@@ -31,6 +34,29 @@
     <el-aside class="bd">
       <form-right :data="widgetFormSelect"></form-right>
     </el-aside>
+    <!-- JSON dialog -->
+    <el-dialog
+      title="页面Json"
+      :visible.sync="getJSONShow"
+      :destroy-on-close="true"
+      :close-on-click-modal="false"
+      width="1200px"
+      center
+    >
+      <div id="jsoneditor" style="height: 400px; width: 100%">
+        {{ jsonTemplate }}
+      </div>
+      <span slot="footer" class="dialog-footer">
+        <el-button
+          class="json-btn"
+          v-clipboard:copy="jsonCopyValue"
+          v-clipboard:success="onCopy"
+          type="primary"
+          >复制JSON</el-button
+        >
+        <el-button @click="downloadJson">下载文件（json）</el-button>
+      </span>
+    </el-dialog>
   </el-container>
 </template>
 
@@ -100,6 +126,12 @@ export default {
       // 中间拖拽表格数据
       widgetForm: { ...defaultJson },
       widgetFormSelect: {}, // 默认是对象
+      // 弹窗显示JSON数据
+      getJSONShow: false,
+      // 需要显示的JSON数据
+      jsonTemplate: '',
+      // 复制的json数据
+      jsonCopyValue: '',
     };
   },
   mounted() {},
@@ -112,6 +144,51 @@ export default {
     clearJson() {
       // console.log('清空元素')
       this.widgetForm = { ...defaultJson };
+    },
+    // 点击复制json
+    getJson() {
+      this.getJSONShow = true;
+      this.jsonTemplate = this.widgetForm;
+      this.$nextTick(() => {
+        // eslint-disable-next-line
+        let editor = ace.edit("jsoneditor");
+        editor.$blockScrolling = Infinity;
+        editor.setFontSize(16);
+        editor.session.setMode("ace/mode/json");
+        editor.setOptions({
+          enableBasicAutocompletion: true,
+          enableSnippets: true,
+          enableLiveAutocompletion: true,
+        });
+        editor.getSession().on("change", function (e) {
+          // 内容变化触发方法
+          console.log(e);
+        });
+        editor.setTheme("ace/theme/monokai");
+        // 记录要复制的数据，注意：必须转字符串
+        this.jsonCopyValue = JSON.stringify(this.widgetForm);
+      });
+    },
+    // 点击复制，触发的方法
+    onCopy () {
+      this.$message.success('复制成功')
+    },
+    downloadJson () {
+      let data = this.jsonCopyValue
+      const filename= 'code.json'
+      if(typeof data === 'object'){
+        // 将json转化为字符串
+        data = JSON.stringify(data, undefined, 4)
+      }
+      var blob = new Blob([data], {type: 'text/json'}),
+      e = document.createEvent('MouseEvents'),
+      a = document.createElement('a')
+      a.download = filename
+      a.href = window.URL.createObjectURL(blob)
+      a.dataset.downloadurl = ['text/json', a.download, a.href].join(':')
+      // 模拟下载文件
+      e.initMouseEvent('click', true, false, window, 0, 0, 0, 0, 0, false, false, false, false, 0, null)
+      a.dispatchEvent(e)
     },
   },
 };
