90a6dfaf6f2d4473412f5f0fe6300d4fc689845a
xiaolin <462441355@qq.com>
2021-05-20 22:39:27 +0800

    * 生成代码

--- src/compile/index.js
+++ src/compile/index.js
@@ -13,4 +13,27 @@ export const compiler = function (list, options = {}) {
         optimize(ast, options)
     }
     let { code, data, events } = genrate.codegen(ast, options)
+    // options.template 专门是为了渲染 .vue整个文件的
+    if (options.template) {
+        let ret = ''
+        // 生成的是template
+        ret = genrate.temgen({ code: code, data: data, events: events }, options)
+        // // console.log('compiler', code, data, events, ret, options)
+        return {
+            ast,
+            code: code, // code 是 特指 html部分
+            data: data, // data
+            events: events, // 事件对象
+            templateStr: ret // 整个文件
+        }
+    } else {
+        // console.log('compiler', code, data, events, options)
+        return {
+            ast,
+            code: code,
+            data: data,
+            events: events
+        }
+    }
+
 }
