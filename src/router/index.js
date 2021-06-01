9811acf5d60426d51908466b18fe1bdcbca44215
xiaolin <462441355@qq.com>
2021-06-01 15:57:41 +0800

    * 路由

--- src/router/index.js
+++ src/router/index.js
@@ -1,14 +1,26 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Home from '../views/Home.vue'
+import devEbs from '../views/devEbs.vue'
+import qq from '../views/qq.vue'
 
 Vue.use(VueRouter)
 
 const routes = [
   {
     path: '/',
-    name: 'Home',
+    name: 'home',
     component: Home
+  },
+  {
+    path: '/ebs',
+    name: 'ebs',
+    component: devEbs
+  },
+  {
+    path: '/qq',
+    name: 'qq',
+    component: qq
   }
 ]
 
